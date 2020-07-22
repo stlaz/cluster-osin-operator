fbb821c52dea7ee06c261bca8cdaaf81fad97208
Stanislav Laznicka <slaznick@redhat.com>
2020-07-22 13:21:24 +0200

    add observer for encryption-config secret

--- pkg/operator/configobservation/listers.go
+++ pkg/operator/configobservation/listers.go
@@ -18,6 +18,7 @@ type Listers struct {
 	APIServerLister_ configlistersv1.APIServerLister
 	ConfigMapLister_ corelistersv1.ConfigMapLister
 	EndpointsLister_ corelistersv1.EndpointsLister
+	SecretLister_    corelistersv1.SecretLister
 
 	ResourceSync       resourcesynccontroller.ResourceSyncer
 	PreRunCachesSynced []cache.InformerSynced
@@ -42,4 +43,8 @@ func (l Listers) ConfigMapLister() corelistersv1.ConfigMapLister {
 
 func (l Listers) EndpointsLister() corelistersv1.EndpointsLister {
 	return l.EndpointsLister_
+}
+
+func (l Listers) SecretLister() corelistersv1.SecretLister {
+	return l.SecretLister_
 }
